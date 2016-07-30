30fc56cf74de1e796b4c778a15194c3065e41d5f
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-07-31 02:01:19 +0900

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import react from "react";
+import reactDom from 'react-dom';
+import _ from 'underscore';
+import $ from 'jquery';
+
+
+function hello() {
+	console.log('hello');
+}
+
+$(function(){
+	"use strict";
+
+	hello();
+
+});
+
